f7ba2b321f67a2e49c23633c60af940864b72fa1
aaelsay3d <170895942+aaelsay3d@users.noreply.github.com>
2024-07-17 21:36:25 +0300

    Complete createGrid function

---
+++ script.js
@@ -0,0 +1,12 @@
+let gridSize = 16;
+
+function createGrid(gridSize){
+    let container = document.querySelector(".container");
+    for (let i = 0; i < gridSize; i++){
+        for (let j = 0; j < gridSize; j++){
+            let squareDiv = document.createElement("div");
+        }
+    }
+}
+
+createGrid(gridSize);
