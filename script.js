8c45f44556e570f5bc4b39c083b48e058a785102
aaelsay3d <170895942+aaelsay3d@users.noreply.github.com>
2024-07-17 23:33:22 +0300

    Change width and height of squares automatically and style them

--- script.js
+++ script.js
@@ -1,10 +1,14 @@
-let gridSize = 16;
+let gridSize = 20;
 
 function createGrid(gridSize){
     let container = document.querySelector(".container");
     for (let i = 0; i < gridSize; i++){
         for (let j = 0; j < gridSize; j++){
             let squareDiv = document.createElement("div");
+            squareDiv.style.width = `${600 / gridSize - 2}px`;
+            squareDiv.style.height = `${600 / gridSize - 2}px`;
+            squareDiv.style.border = "1px dotted grey";
+            container.appendChild(squareDiv);
         }
     }
 }
